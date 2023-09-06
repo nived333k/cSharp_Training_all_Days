c7ce57e85691378fd5074e2f1273c064a9b28f2a
nived333k <nived14k@gmail.com>
2023-09-06 13:38:49 +0530

    this is commit

---
+++ ConsoleApp1/ConsoleApp1/CreateClassObject.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class CreateClassObject
+    {
+         public static void Main(string[] args)
+        {
+            CreateClassObject cobj = new CreateClassObject();
+            
+            CreateClassObject nived = new CreateClassObject();
+            nived.info();
+
+            //Console.WriteLine("the sum of the vaues are "+ cobj.add(2, 4));
+            //cobj.esakki();
+
+           // Console.ReadLine();
+        }
+        
+
+        public  int  add(int a , int b)
+        {
+            return a + b;
+        }
+
+        public  void esakki()
+        {
+            
+            
+            Console.WriteLine("Emotional damage is my Vibe!!!!");
+
+        }
+        public  void info()
+        {
+            
+            Console.WriteLine("enter your Name");
+            string name = Console.ReadLine();
+            Console.WriteLine("your age ");
+            int age = Convert.ToInt32(Console.Read());
+            Console.WriteLine( "your emp id");
+            int color = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("color is:", +color);
+            Console.WriteLine($"Your employee information is {name} your age is {age} your emp_no {color}");
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ Day3Training/Day3Training/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3Training
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            int prod = 0;
+            int diff = 0;
+            int div = 0;
+            Program pobj = new Program();
+            Program.Calculate(10, 5, out sum, out prod, out diff, out div);
+        }
+        public static void Calculate(int a ,int b ,out int sum , out int product , out int difference, out int division)
+                
+        {
+
+            Console.WriteLine(sum = a + b);
+            Console.WriteLine(product = a * b);
+            Console.WriteLine(division = a / b);
+            Console.WriteLine(difference = a-b);
+            Console.Read();
+        }
+    }
+}
