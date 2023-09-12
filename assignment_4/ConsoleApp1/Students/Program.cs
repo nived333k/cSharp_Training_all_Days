ce0644a540294547cb800fdf116e424bc39206df
nived333k <nived14k@gmail.com>
2023-09-12 22:40:08 +0530

    assignment 4

---
+++ assignment_4/ConsoleApp1/Students/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Students
+{
+    class student
+    {
+
+        public string rollno;
+        public string name;
+        public string Class;
+        public string sem;
+        public string branch;
+        public int[] marks = new int[5]; // for marks of 5 subject
+
+
+        //lets create a constructor
+        //constructor should have the same class name
+
+        public student(string rollno , string name , string Class , string sem , string branch  )
+        {
+            this.rollno = rollno;
+            this.name = name;
+            this.Class = Class;
+            this.sem = sem;
+            this.branch = branch;
+
+
+
+        }
+        //method getmark()
+
+        public void getMark()
+        {
+            Console.WriteLine("enter the marks for all 5 subjects");
+            for (int i = 0; i < marks.Length; i++)
+            {
+                Console.WriteLine("enter"+(i+1)+"marks");
+                marks[i] = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+
+        // create a function to calculate average 
+
+        public double calAvg()
+        {
+            double totalmarks = 0;
+            for(int i=0; i<marks.Length; i++)
+            {
+                totalmarks = totalmarks + marks[i];
+            }
+            return totalmarks / 5;
+        }
+
+        public void displayReslt()
+        {
+            double averageMarks = calAvg();
+
+            for(int i=0; i<marks.Length; i++)
+            {
+                if (marks[i] < 35)
+                {
+                    Console.WriteLine("mark less than 35 : failed");
+                    Console.ReadLine();
+                    break;
+                }
+               else  if (marks[i] > 35 && averageMarks < 50)
+                {
+                    Console.WriteLine("mark is greater than 35 but average is less than 50 :  failed");
+                    Console.ReadLine();
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("when all marke greater than 35 and average greater than 50 : passed");
+                    Console.ReadLine();
+                    break;
+                }
+            }
+
+
+            //for (int i = 0; i < marks.Length; i++)
+            //{
+            //    if(marks[i] > 35 && averageMarks < 50)
+            //    {
+            //        Console.WriteLine("mark is greater than 35 but average is less than 50 :  failed");
+            //        Console.ReadLine();
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("when all marke greater than 35 and average greater than 50 : passed");
+            //        Console.ReadLine();
+            //    }
+            //}
+        }
+
+        public void displaydata()
+        {
+            Console.WriteLine($"roll no {rollno} \n name {name} \n Class {Class} \n sem {sem} \n branch {branch} \n marks ");
+            for(int i=0; i < marks.Length; i++)
+            {
+                Console.WriteLine($"{   marks[i]    }");
+
+            }
+            Console.ReadLine();
+
+        }
+
+
+        static void Main(string[] args)
+        {
+            student stud1 = new student("1033385", "nived k ", "2", "b","ise");
+            stud1.getMark();
+            stud1.displayReslt();
+            stud1.displaydata();
+
+        }
+    }
+}
---
+++ assignment_4/ConsoleApp1/accounts/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace accounts
+{
+    
+     internal class accounts
+    {
+
+        public int accountNo;
+        public string customerName;
+        public string accountType;
+        public string transactionType;
+        public double amount;
+        public double balance = 15000;
+
+        public void userScreen()
+        {
+            Console.WriteLine("enter account number  ");
+            this.accountNo = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("enter the customer name  ");
+            this.customerName = Console.ReadLine();
+
+            Console.WriteLine("enter the account type");
+            this.accountType = Console.ReadLine();
+
+            Console.WriteLine("enter your transaction type. you can type d or w which is deposit or withdrawal");
+            this.transactionType = Console.ReadLine();
+
+            Console.WriteLine("enter the amount you want to withdraw or deposit ");
+            this.amount = Convert.ToDouble(Console.ReadLine());
+
+
+            // now we will run a if else loop accordingly what user enter if its deposit or withdraw .
+            // but the withdraw amount should be less than the balance which is 15k.
+
+            if (transactionType == "d")
+            {
+
+                balance = balance + amount;
+
+            }
+            else if (transactionType == "w" && amount < balance)
+            {
+                balance = balance - amount;
+            }
+            else if (transactionType == "w" && amount > balance)
+            {
+                Console.WriteLine("Balance is low please check ");
+            }
+
+            Console.WriteLine($" 1.account number = {accountNo} \n 2.customer name = {customerName} \n 3.account type = {accountType} \n 4.transaction type = {transactionType} \n 5. amount = {amount} \n 6.balance ={balance}");
+            Console.ReadLine();
+        }
+
+        static void Main(string[] args)
+        {
+            accounts user = new accounts();
+            user.userScreen();
+
+        }
+
+    }
+}
