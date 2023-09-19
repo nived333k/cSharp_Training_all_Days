b6319583a581607a64f2a7038a11648568e04a05
nived333k <nived14k@gmail.com>
2023-09-19 11:08:27 +0530

    exam 2

---
+++ Csharp_code_Base_Exam2/question1/question1/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace question1
+{
+
+
+     abstract class student 
+    {
+        public string  stud_name { get; set; }
+        public int stud_id { get; set; }
+        public int stud_grade { get; set; }
+
+        public student(string student_name , int student_id , int student_grade)
+        {
+             stud_name = student_name;
+             stud_id  = student_id;
+             stud_grade = student_grade;
+
+        }
+        public abstract bool isPassed(int student_grade);
+
+    }
+
+    class undergrads:student
+    {
+        public undergrads(string student_name , int student_id  , int student_grade) : base(student_name,student_id,student_grade)
+        {
+
+        }
+        public override bool isPassed(int student_grade)
+        {
+            return student_grade > 70;
+        }
+
+    }
+
+    class graduate : student 
+    {
+        public graduate(string student_name, int student_id, int student_grade) : base(student_name, student_id, student_grade)
+        {
+
+        }
+        public override bool isPassed(int student_grade)
+        {
+            return stud_grade > 80;
+
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            student ungrad = new undergrads("Nived",1033385,80);
+            Console.WriteLine($"{ungrad.stud_name} has passed {ungrad.isPassed(ungrad.stud_grade)}");
+            student grad = new graduate("NIved", 1033385, 60);
+            Console.WriteLine($"{grad.stud_name} has passed {grad.isPassed(grad.stud_grade)}");
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Csharp_code_Base_Exam2/question1/question1/question2.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace question1
+{
+
+    class product
+    {
+        //we are creating properties
+        public int product_id { get; set; }
+        public string product_name { get; set; }
+        public double product_price { get; set; }
+
+        //create a constructor for the properties
+        public product(int pid, string pname, double pprice)
+        {
+            product_id = pid;
+            product_name = pname;
+            product_price = pprice;
+
+        }
+
+    }
+
+    class question2
+    {
+        static void Main()
+        {
+            List<product> products_list = new List<product>();
+
+            //to accept 10 product
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine("enter products id");
+                i = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("enter the name of products");
+                string product_name = Console.ReadLine();
+                Console.WriteLine("enter the price of items");
+                double product_price = Convert.ToDouble(Console.ReadLine());
+                products_list.Add(new product(i, product_name, product_price));
+            }
+            // to sort product by price
+            products_list = products_list.OrderBy(p => p.product_price).ToList();
+
+            //sort products
+            Console.WriteLine("sorted by price");
+            foreach (var prod in products_list)
+            {
+                Console.WriteLine($"1.productid {prod.product_id} \n 2.productname  {prod.product_name} \n 3.productprice{prod.product_price}");
+            }
+            Console.ReadLine();
+        }
+
+    }
+}
