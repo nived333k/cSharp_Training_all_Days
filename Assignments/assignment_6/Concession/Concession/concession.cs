d33d6d25066a3526e1193f8b830b7a3959ec67e4
nived333k <nived14k@gmail.com>
2023-09-23 21:05:46 +0530

    assignment_6

---
+++ Assignments/assignment_6/Concession/Concession/concession.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Concession
+{
+    public class conscession
+    {
+        public  int TicketPrice = 500;
+        public string name;
+        public int age;
+
+        public void calculateConsession()
+        {
+            if (age <= 5)
+            {
+                Console.WriteLine("your name is {0} and your age is {1} Little champ you have won a free ticket ",name,age);
+            }
+            else if (age > 60)
+            {
+                double TicketFair = TicketPrice * 0.30;
+                double discountprice = TicketPrice - TicketFair;
+                Console.WriteLine("senior citizen ticket price {0} \n your name is {1} your age is {2}",discountprice,name,age);
+            }
+
+            else
+            {
+                Console.WriteLine("your tickets are booked {0} and your name is {1} and your age is {2}",TicketPrice,name,age);
+            }
+
+        }
+        
+    }
+
+}
---
+++ Assignments/assignment_6/Concession/ConsessionApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Concession;
+
+namespace ConsessionApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter your name ");
+            string Name = Console.ReadLine();
+
+            Console.WriteLine("enter your age");
+            int Age = Convert.ToInt32(Console.ReadLine());
+
+            Concession.conscession calculator = new Concession.conscession
+            {
+                name = Name,
+                age = Age
+            };
+            calculator.calculateConsession();
+            Console.ReadLine();
+
+
+        }
+    }
+}
