9d416b8d8831a5d978467ab7750f9c5b5ce84c26
nived333k <nived14k@gmail.com>
2023-09-27 22:41:36 +0530

    assignment 7

---
+++ Assignments/assignment_7/tcpipclient/tcpipclient/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿
+namespace tcpipclient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.num1 = new System.Windows.Forms.Label();
+            this.num2 = new System.Windows.Forms.Label();
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.txt2 = new System.Windows.Forms.TextBox();
+            this.btn = new System.Windows.Forms.Button();
+            this.result = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // num1
+            // 
+            this.num1.AutoSize = true;
+            this.num1.Location = new System.Drawing.Point(124, 144);
+            this.num1.Name = "num1";
+            this.num1.Size = new System.Drawing.Size(96, 13);
+            this.num1.TabIndex = 0;
+            this.num1.Text = "enter the number 1";
+            // 
+            // num2
+            // 
+            this.num2.AutoSize = true;
+            this.num2.Location = new System.Drawing.Point(124, 181);
+            this.num2.Name = "num2";
+            this.num2.Size = new System.Drawing.Size(96, 13);
+            this.num2.TabIndex = 1;
+            this.num2.Text = "enter the number 2";
+            this.num2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // txt1
+            // 
+            this.txt1.Location = new System.Drawing.Point(241, 141);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(100, 20);
+            this.txt1.TabIndex = 2;
+            // 
+            // txt2
+            // 
+            this.txt2.Location = new System.Drawing.Point(241, 181);
+            this.txt2.Name = "txt2";
+            this.txt2.Size = new System.Drawing.Size(100, 20);
+            this.txt2.TabIndex = 3;
+            // 
+            // btn
+            // 
+            this.btn.Location = new System.Drawing.Point(185, 233);
+            this.btn.Name = "btn";
+            this.btn.Size = new System.Drawing.Size(131, 23);
+            this.btn.TabIndex = 4;
+            this.btn.Text = "HighestNumber";
+            this.btn.UseVisualStyleBackColor = true;
+            this.btn.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // result
+            // 
+            this.result.Location = new System.Drawing.Point(403, 159);
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(100, 20);
+            this.result.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.btn);
+            this.Controls.Add(this.txt2);
+            this.Controls.Add(this.txt1);
+            this.Controls.Add(this.num2);
+            this.Controls.Add(this.num1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label num1;
+        private System.Windows.Forms.Label num2;
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.TextBox txt2;
+        private System.Windows.Forms.Button btn;
+        private System.Windows.Forms.TextBox result;
+    }
+}
+
---
+++ Assignments/assignment_7/tcpipclient/tcpipclient/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using tcpserver;
+using System.Runtime.Remoting.Channels;
+
+
+
+namespace tcpipclient
+{
+    public partial class Form1 : Form
+    {
+        service remoteobj = new service();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_Click(object sender, EventArgs e)
+        {
+            remoteobj = (service)Activator.GetObject(typeof(service),"tcp://localhost:8089/ourfirstremoteservice");
+            int n1 = Int32.Parse(txt1.Text);
+            int n2 = Int32.Parse(txt2.Text);
+            result.Text=remoteobj.highestnumber(n1,n2).ToString();
+        }
+    }
+}
