c1a8f3c30dd74e8f197d9504ea455ef4234e4223
nived333k <nived14k@gmail.com>
2023-10-18 12:43:14 +0530

    assignment_5

---
+++ All_Day_training_c#/New folder/ado/ado/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace ado
+{
+    class Program
+    {
+        public static SqlConnection con;
+        public static SqlCommand cmd;
+        public static SqlDataReader dr;
+        static void Main(string[] args)
+        {
+            // SelectData();
+            //query();
+            insert();
+      
+        }
+
+
+        static void SelectData()
+        {
+            con = GetConnection();
+            //string cs = @"data source=ICS-LT-83GB893\SQLEXPRESS;database =assignment_2; integrated security=SSPI";
+            //SqlConnection con = new SqlConnection(cs);
+            con.Open();
+            SqlCommand cmd = new SqlCommand("select * from emp", con);
+            SqlDataReader dr = cmd.ExecuteReader();
+
+            while (dr.Read())
+            {
+                Console.WriteLine("empno :" + " " + dr[0]);
+                Console.WriteLine("ename :" + " " + dr[1]);
+                Console.WriteLine("ejob :" + " " + dr[2]);
+                Console.WriteLine("emgr_id :" + " " + dr[3]);
+                Console.WriteLine("ehire_date :" + " " + dr[4]);
+                Console.WriteLine("salary :" + " " + dr[5]);
+                Console.WriteLine("comm :" + " " + dr[6]);
+                Console.WriteLine("deptno :" + " " + dr[7]);
+                Console.WriteLine("---------------------------");
+
+            }
+            dr.Close();
+            con.Close();
+            Console.ReadLine();
+
+        }
+        static SqlConnection GetConnection()
+        {
+            string cs = @"data source=ICS-LT-83GB893\SQLEXPRESS;database =assignment_2; integrated security=SSPI";
+            SqlConnection con = new SqlConnection(cs);
+            return con;
+
+        }
+
+
+        static void query()
+        {
+            con = GetConnection();
+            SqlCommand cmd = new SqlCommand("select COUNT(ename) from emp", con);
+            con.Open();
+            int totalrows = (int)cmd.ExecuteScalar();
+            Console.WriteLine("Total rows = "+totalrows.ToString());
+            Console.ReadLine();
+
+        }
+
+        static void insert()
+        {
+            con = GetConnection();
+            cmd = new SqlCommand("insert into emp values (8981,'nnk','well wisher',3456,'1901-12-20',90000,null,20)", con);
+            con.Open();
+            int totalrowsinserted = cmd.ExecuteNonQuery();
+            Console.WriteLine("total rows inserted = "+totalrowsinserted);
+            Console.ReadLine();
+
+        }
+    }
+        
+}
---
+++ SQL_Assignments/assignment_5.sql
@@ -0,0 +1,41 @@
+
+CREATE PROCEDURE Payslip
+@EmployeeID INT,
+@Salary DECIMAL(10, 2)
+AS
+BEGIN
+    DECLARE @HRA DECIMAL(10, 2)
+    DECLARE @DA DECIMAL(10, 2)
+	DECLARE @PF DECIMAL(10, 2)
+    DECLARE @IT DECIMAL(10, 2)
+    DECLARE @Deductions DECIMAL(10, 2)
+    DECLARE @GrossSalary DECIMAL(10, 2)
+    DECLARE @NetSalary DECIMAL(10, 2)
+    -- to calculate the HRA, DA, PF, and IT
+
+    SET @HRA = 0.10 * @Salary
+    SET @DA = 0.20 * @Salary
+    SET @PF = 0.08 * @Salary
+    SET @IT = 0.05 * @Salary
+    -- to calculate the Deductions
+    SET @Deductions = @PF + @IT
+    -- to calculate Gross Salary
+    SET @GrossSalary = @Salary + @HRA + @DA
+    -- Calculate Net Salary
+    SET @NetSalary = @GrossSalary - @Deductions
+    -- Print the payslip
+    PRINT 'Employee Payslip for EmployeeID: ' + CAST(@EmployeeID AS VARCHAR(10))
+    PRINT 'Basic Salary: ' + CAST(@Salary AS VARCHAR(10))
+    PRINT 'HRA: ' + CAST(@HRA AS VARCHAR(10))
+    PRINT 'DA: ' + CAST(@DA AS VARCHAR(10))
+    PRINT 'PF: ' + CAST(@PF AS VARCHAR(10))
+    PRINT 'IT: ' + CAST(@IT AS VARCHAR(10))
+    PRINT 'Deductions: ' + CAST(@Deductions AS VARCHAR(10))
+    PRINT 'Gross Salary: ' + CAST(@GrossSalary AS VARCHAR(10))
+    PRINT 'Net Salary: ' + CAST(@NetSalary AS VARCHAR(10))
+   
+END
+
+
+
+EXEC Payslip @EmployeeID = 7789, @Salary = 60000;
