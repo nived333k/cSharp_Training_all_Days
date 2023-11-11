91818c161f86265fe59ccca1a5aecd1b1de3fadf
nived333k <nived14k@gmail.com>
2023-11-11 08:04:56 +0530

    update

--- Asp_MVC_codetest/codefirst/codefirst/Controllers/HomeController.cs
+++ Asp_MVC_codetest/codefirst/codefirst/Controllers/HomeController.cs
@@ -3,28 +3,62 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+using codefirst.Models;
 
 namespace codefirst.Controllers
 {
     public class HomeController : Controller
     {
+        private MoviesDbContext _dbContext = new MoviesDbContext();
+
         public ActionResult Index()
         {
+            var movies = _dbContext.Movie.ToList();
+            return View(movies);
+        }
+
+        public ActionResult Create()
+        {
             return View();
         }
 
-        public ActionResult About()
+        [HttpPost]
+        public ActionResult Create(movies movie)
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
+            _dbContext.Movie.Add(movie);
+            _dbContext.SaveChanges();
+            return RedirectToAction("Index");
         }
 
-        public ActionResult Contact()
+        public ActionResult Edit(int id)
         {
-            ViewBag.Message = "Your contact page.";
+            var movie = _dbContext.Movie.Find(id);
+            return View(movie);
+        }
 
-            return View();
+        [HttpPost]
+        public ActionResult Edit(movies movie)
+        {
+            _dbContext.Entry(movie).State = EntityState.Modified;
+            _dbContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult Delete(int id)
+        {
+            var movie = _dbContext.Movie.Find(id);
+            return View(movie);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            var movie = _dbContext.Movie.Find(id);
+            _dbContext.Movie.Remove(movie);
+            _dbContext.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
+
 }
---
+++ Asp_MVC_codetest/datafirst/datafirst/Models/moviedb.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace datafirst.Models
+{
+    public class MoviesDbContext : DbContext
+    {
+        public DbSet<movies> Movies { get; set; }
+    }
+}
